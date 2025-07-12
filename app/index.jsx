be19302e84f2dbd1e8b8807449f374bfa34a7d38
Jad-jhayem <jadgehayem@gmail.com>
2025-07-12 15:42:06 +0300

    cleaned linkContaner css class in index.js

--- app/index.jsx
+++ app/index.jsx
@@ -90,13 +90,5 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 20,
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
   },
 });
